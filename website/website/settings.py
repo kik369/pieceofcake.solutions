6fd682ae8427383c2137038b6ebe063650e9c7a5
Kris <kristapsk@gmail.com>
2020-07-06 17:17:42 +0100

    test error

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='159.65.90.34', cast=Csv())
-
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='159.65.90.34', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
